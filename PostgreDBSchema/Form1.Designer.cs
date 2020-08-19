7bf8835e904cf58e8f548fdd0805507859f56342
dmb-dev87 <dmbdev800@gmail.com>
2020-08-19 00:11:49 -0600

    [TopOneExpert][2020-08-19][build project]

---
+++ PostgreDBSchema/Form1.Designer.cs
@@ -0,0 +1,226 @@
+﻿namespace PostgreDBSchema
+{
+    partial class Schema_Form
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.txt_ipaddr = new System.Windows.Forms.TextBox();
+            this.txt_portnum = new System.Windows.Forms.TextBox();
+            this.txt_username = new System.Windows.Forms.TextBox();
+            this.txt_password = new System.Windows.Forms.TextBox();
+            this.btn_start = new System.Windows.Forms.Button();
+            this.btn_close = new System.Windows.Forms.Button();
+            this.label_status = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.txt_dbname = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(-381, 48);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(35, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "label1";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(39, 33);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(111, 24);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "IP Address: ";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(39, 75);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(122, 24);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "Port Number:";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.Location = new System.Drawing.Point(39, 117);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(110, 24);
+            this.label4.TabIndex = 3;
+            this.label4.Text = "User Name:";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label5.Location = new System.Drawing.Point(39, 159);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(97, 24);
+            this.label5.TabIndex = 4;
+            this.label5.Text = "Password:";
+            // 
+            // txt_ipaddr
+            // 
+            this.txt_ipaddr.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txt_ipaddr.Location = new System.Drawing.Point(186, 30);
+            this.txt_ipaddr.Name = "txt_ipaddr";
+            this.txt_ipaddr.Size = new System.Drawing.Size(166, 29);
+            this.txt_ipaddr.TabIndex = 5;
+            // 
+            // txt_portnum
+            // 
+            this.txt_portnum.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txt_portnum.Location = new System.Drawing.Point(186, 72);
+            this.txt_portnum.Name = "txt_portnum";
+            this.txt_portnum.Size = new System.Drawing.Size(166, 29);
+            this.txt_portnum.TabIndex = 6;
+            // 
+            // txt_username
+            // 
+            this.txt_username.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txt_username.Location = new System.Drawing.Point(186, 114);
+            this.txt_username.Name = "txt_username";
+            this.txt_username.Size = new System.Drawing.Size(166, 29);
+            this.txt_username.TabIndex = 7;
+            // 
+            // txt_password
+            // 
+            this.txt_password.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txt_password.Location = new System.Drawing.Point(186, 156);
+            this.txt_password.Name = "txt_password";
+            this.txt_password.PasswordChar = '*';
+            this.txt_password.Size = new System.Drawing.Size(166, 29);
+            this.txt_password.TabIndex = 8;
+            // 
+            // btn_start
+            // 
+            this.btn_start.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btn_start.ForeColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.btn_start.Location = new System.Drawing.Point(230, 301);
+            this.btn_start.Name = "btn_start";
+            this.btn_start.Size = new System.Drawing.Size(122, 30);
+            this.btn_start.TabIndex = 10;
+            this.btn_start.Text = "START";
+            this.btn_start.UseVisualStyleBackColor = true;
+            this.btn_start.Click += new System.EventHandler(this.btn_start_Click);
+            // 
+            // btn_close
+            // 
+            this.btn_close.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btn_close.ForeColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.btn_close.Location = new System.Drawing.Point(230, 337);
+            this.btn_close.Name = "btn_close";
+            this.btn_close.Size = new System.Drawing.Size(122, 27);
+            this.btn_close.TabIndex = 13;
+            this.btn_close.Text = "CLOSE";
+            this.btn_close.UseVisualStyleBackColor = true;
+            this.btn_close.Click += new System.EventHandler(this.btn_close_Click);
+            // 
+            // label_status
+            // 
+            this.label_status.AutoSize = true;
+            this.label_status.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label_status.Location = new System.Drawing.Point(39, 259);
+            this.label_status.Name = "label_status";
+            this.label_status.Size = new System.Drawing.Size(0, 20);
+            this.label_status.TabIndex = 11;
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label6.Location = new System.Drawing.Point(39, 201);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(96, 24);
+            this.label6.TabIndex = 12;
+            this.label6.Text = "DB Name:";
+            // 
+            // txt_dbname
+            // 
+            this.txt_dbname.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txt_dbname.Location = new System.Drawing.Point(186, 198);
+            this.txt_dbname.Name = "txt_dbname";
+            this.txt_dbname.Size = new System.Drawing.Size(166, 29);
+            this.txt_dbname.TabIndex = 9;
+            // 
+            // Schema_Form
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ControlDarkDark;
+            this.ClientSize = new System.Drawing.Size(398, 395);
+            this.Controls.Add(this.txt_dbname);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label_status);
+            this.Controls.Add(this.btn_close);
+            this.Controls.Add(this.btn_start);
+            this.Controls.Add(this.txt_password);
+            this.Controls.Add(this.txt_username);
+            this.Controls.Add(this.txt_portnum);
+            this.Controls.Add(this.txt_ipaddr);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.ForeColor = System.Drawing.SystemColors.Control;
+            this.Name = "Schema_Form";
+            this.Text = "DB Schema Change";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.TextBox txt_ipaddr;
+        private System.Windows.Forms.TextBox txt_portnum;
+        private System.Windows.Forms.TextBox txt_username;
+        private System.Windows.Forms.TextBox txt_password;
+        private System.Windows.Forms.Button btn_start;
+        private System.Windows.Forms.Button btn_close;
+        private System.Windows.Forms.Label label_status;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.TextBox txt_dbname;
+    }
+}
+
---
+++ PostgreDBSchema/Form1.cs
@@ -0,0 +1,232 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Windows.Forms;
+using Npgsql;
+using System.IO;
+
+namespace PostgreDBSchema
+{
+    public partial class Schema_Form : Form
+    {
+        private string str_ipaddr;
+        private string str_portNum;
+        private string str_usrName;
+        private string str_password;
+        private string str_dbName;
+        private NpgsqlConnection db_conn;
+
+        private StreamWriter sw;
+
+        public Schema_Form()
+        {
+            InitializeComponent();
+
+            try
+            {
+                //Pass the filepath and filename to the StreamWriter Constructor
+                sw = new StreamWriter("log.txt");
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine("Exception: " + e.Message);
+            }
+            finally
+            {
+                Console.WriteLine("Executing finally block.");
+            }
+        }
+
+        private bool validateValues()
+        {
+            if (txt_ipaddr.Text == "")
+            {
+                MessageBox.Show("Please enter the ip address of Database.", "Information", MessageBoxButtons.OK);
+                return false;
+            }
+            else
+            {
+                str_ipaddr = txt_ipaddr.Text;
+            }
+
+            if (txt_portnum.Text == "")
+            {
+                MessageBox.Show("Please enter the port number of Database.", "Information", MessageBoxButtons.OK);
+                return false;
+            }
+            else
+            {
+                str_portNum = txt_portnum.Text;
+            }
+
+            if (txt_username.Text == "")
+            {
+                MessageBox.Show("Please input the user name of Database.", "Information", MessageBoxButtons.OK);
+                return false;
+            }
+            else
+            {
+                str_usrName = txt_username.Text;
+            }
+
+            if (txt_password.Text == "")
+            {
+                MessageBox.Show("Please input the password of Database.", "Information", MessageBoxButtons.OK);
+                return false;
+            }
+            else
+            {
+                str_password = txt_password.Text;
+            }
+
+            if (txt_dbname.Text == "")
+            {
+                MessageBox.Show("Please input the name of Database.", "Information", MessageBoxButtons.OK);
+                return false;
+            }
+            else
+            {
+                str_dbName = txt_dbname.Text;
+            }
+
+            return true;
+        }
+
+        private bool connect_db()
+        {
+            try
+            {
+                db_conn = new NpgsqlConnection("Server=" + str_ipaddr + "; Port=" + str_portNum + "; User Id=" + str_usrName + "; Password=" + str_password + "; Database=" + str_dbName);
+                label_status.Text = "DB Connection: Success.";
+            }
+            catch(Exception ex)
+            {
+                label_status.Text = "Error: " + ex.ToString();
+                return false;
+            }            
+
+            return true;
+        }
+
+        private void btn_start_Click(object sender, EventArgs e)
+        {
+            bool val_data = validateValues();
+
+            bool con_db = false;
+            
+            if (val_data)
+            {
+                con_db = connect_db();
+            }
+
+            if (con_db)
+            {
+                processTables();
+            }
+        }
+
+        private void processTables()
+        {
+            string value = null;
+            List<string> names = new List<string>();
+
+            db_conn.Open();
+
+            NpgsqlCommand command = new NpgsqlCommand("SELECT table_name FROM information_schema.tables WHERE table_schema='public'", db_conn);
+            NpgsqlDataReader dr = command.ExecuteReader();
+
+            //change table name
+            while (dr.Read())
+            {
+                value = dr.GetString(0);
+                names.Add(value);
+            }
+
+            db_conn.Close();
+
+            //change table names
+            processTables(names);
+
+            label_status.Text = "Process Success";
+
+            sw.Close();
+
+            return;
+        }
+
+        private void processTables(List<string> names)
+        {
+            for (int i = 0; i < names.Count; i++)
+            {
+                label_status.Text = "Process the table: " + names[i];
+
+                processColumns(names[i]);
+
+                db_conn.Open();
+
+                string new_name = names[i] + "2020";
+                NpgsqlCommand changCmd = new NpgsqlCommand();
+                changCmd.Connection = db_conn;
+                changCmd.CommandText = "ALTER TABLE public." + names[i] + " RENAME TO " + new_name;
+                changCmd.ExecuteNonQuery();
+
+                sw.WriteLine(str_dbName + ", " + names[i] + " : Change tabel name to " + new_name + ".");
+
+                db_conn.Close();
+            }
+        }
+
+        private void processColumns(string tableName)
+        {
+            string value = null;
+            List<string> names = new List<string>();
+
+            db_conn.Open();
+
+            NpgsqlCommand command = new NpgsqlCommand("SELECT column_name FROM information_schema.columns WHERE table_name='" + tableName + "'", db_conn);
+            NpgsqlDataReader dr = command.ExecuteReader();
+
+            //get column names
+            while (dr.Read())
+            {
+                value = dr.GetString(0);
+                names.Add(value);
+            }
+
+            db_conn.Close();
+
+            //change column names
+            changeColumnNames(names, tableName);
+            //changeColumnOrder()
+            return;
+        }
+
+        private void changeColumnNames(List<string> names, string tableName)
+        {
+            db_conn.Open();
+            
+            for (int i = 0; i < names.Count; i++)
+            {
+                string new_name = names[i] + "2020";
+                NpgsqlCommand changCmd = new NpgsqlCommand();
+                changCmd.Connection = db_conn;
+                changCmd.CommandText = "ALTER TABLE " + tableName + " RENAME " + names[i] + " TO " + new_name;
+                changCmd.ExecuteNonQuery();
+
+                sw.WriteLine(str_dbName + ", " + tableName + ", " + names[i] + " : Change column name to " + new_name + ".");
+            }
+
+            db_conn.Close();
+        }
+
+        private void btn_close_Click(object sender, EventArgs e)
+        {
+            Close();
+        }
+    }
+}
